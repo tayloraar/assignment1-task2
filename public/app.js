91f4b5883266110d531968029787989f12214dc9
Aaron Taylor <tayloraar@deakin.edu.au>
2020-08-11 00:53:09 +1000

    refactored code

--- public/app.js
+++ public/app.js
@@ -1,61 +1,60 @@
-  $(document).ready(function(){
-    console.log("Document ready");
-    $('.sidenav').sidenav();
-//On Doucment Load - Posts
 
-       $.get("/postnum", function(posts){
-           $("#mainColum").empty();
-           let postCount = posts.length
-           console.log(postCount)
-            posts.forEach((posts)=>{
-              let id = posts.postNum
-                 $("#mainColum").prepend(`<div class="row post">
-                 <div class="col s12">
-                   <div class="card">
-                     <div class="card-action">
-                      <a class="btn-floating btn-small waves-effect waves-light red" href="#"><img class="circle responsive-img" style= "height:inherit;" src="_images/profile1.jpeg"></a>
-                      <a href="#" class="userName">${posts.user}</a>
-                      </div>
-                       <div class="divider"></div>
-                     <div class="card-image">
-                       <img class="postImage" src="${posts.image}">
-                     </div>
-                     <div class="divider"></div>
-                     <div class="card-content">
-                       <div class="postCaption">${posts.caption}</div>
-                     </div>
-                     <div class="divider"></div>
-                     <div class="divider"></div>
-                     <div class="card-content">
-                       <div class="postComments">
-                       <div class="commentSection" id="${posts.postNum}"></div></div>
-                     </div>
-                     <div class="divider"></div>
-                     <div class="row">
-                         <div class="card-action">
-                         <div class="input-field col s12">
-                           <input id="input${posts.postNum}" type="text" class="validate">
-                           <label class="active" for="input${posts.postNum}">Message</label>
-                           <a id="btnComment${posts.postNum}" class="waves-effet waves-light btn">Post</a>
-                         </div>
-                     </div>
-                     </div>
-                   </div>
-                 </div>
-               </div>`);
-               
-// setInterval(()=>{
- 
-//   if ($('.helper-text').attr(data-success, true)){
-//    // console.log('true')
-//     $('#postBtn').prop('disable', true);
-//   } else{
-//     $('#postBtn').prop('disable', false);
-//     console.log('false')
-//   }
+// This is the main script for the application that will run after the document is read.
+$(document).ready(function(){
 
-// },100);
-              //  New Post
+  // Tell us the document is ready and set up 'sidenav' for the new post window.
+  console.log("Document ready");
+  $('.sidenav').sidenav();
+
+  // Load - posts, set up HTML, link buttons and provide constant pull for messages.
+  $.get("/postnum", function(posts){
+    $("#mainColum").empty();
+    let postCount = posts.length;
+
+    /* This will populate the page with the posts, pushing through the posts and adding in id's and informaiton where needed. 
+    I've also nested some other functions that use the posts.*/
+    posts.forEach((posts)=>{
+      let id = posts.postNum;
+
+      $("#mainColum").prepend(`
+        <div class="row post">
+          <div class="col s12">
+            <div class="card">
+              <div class="card-action">
+                <a class="btn-floating btn-small waves-effect waves-light red" href="#"><img class="circle responsive-img" style= "height:inherit;" src="_images/profile1.jpeg"></a>
+                <a href="#" class="userName">${posts.user}</a>
+              </div>
+              <div class="divider"></div>
+              <div class="card-image">
+                <img class="postImage" src="${posts.image}">
+              </div>
+              <div class="divider"></div>
+              <div class="card-content">
+                <h5>Caption</h5>
+                <div class="postCaption">${posts.caption}</div>
+              </div>
+              <div class="divider"></div>
+              <div class="card-content">
+                <div class="postComments">
+                  <h5>Comments</h5>
+                  <div class="card-content" id="${posts.postNum}"></div>
+                </div>
+              </div>
+              <div class="divider"></div>
+              <div class="row">
+                <div class="card-content">
+                  <div class="input-field col s12">
+                    <input id="input${posts.postNum}" type="text" class="validate">
+                    <label class="active" for="input${posts.postNum}">Message</label>
+                    <a id="btnComment${posts.postNum}" class="waves-effet waves-light btn">Post</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>`);
+
+      // New Post
                $(`#postBtn`).click(function(){
                   console.log(`You hit post button`)
                   let user = $(`#userNameIn`).val();
@@ -80,35 +79,38 @@
                   $(`#userNameIn`).val(" ");
                   $(`#imageUrl`).val(" ");
                   $(`#captionIn`).val(" ");
+                  location.reload();
   
             });
 
-              //New Comment
-               $(`#btnComment${id}`).click(function(){
-                console.log(`You hit button id #btnComment${id}`)
-                let comment = $(`#input${posts.postNum}`).val();
-                let commentId = id
-                let data={
-                    comment,
-                    commentId
-                };
-                console.log(data)
-                $.get("/comment", data,function(){
-        
-                });
-                $(`#input${posts.postNum}`).val(" ");
-            });
-              //Constant Pull New Comments
-               setInterval(()=>{       
-       $.get("/comments", function(comments){
-            $(`#${posts.postNum}`).empty();
-            comments.forEach((comment)=>{
-             if (comment.commentId == id){
-             $(`#${id}`).append(`<div class='row'>${comment.comments}</div>`);
-            };
-          })
+      // New Comment
+      $(`#btnComment${id}`).click(function(){
+        console.log(`You hit button id #btnComment${id}`);
+        let comment = $(`#input${posts.postNum}`).val();
+        let commentId = id;
+        let data={
+          comment,
+          commentId
+        }
+
+        $.get("/comment", data,function(){
+
         });
-      },1000)
-      })
+
+        $(`#input${posts.postNum}`).val(" ");
+      });
+
+      // Constant Pull New Comments
+      setInterval(()=>{       
+        $.get("/comments", function(comments){
+          $(`#${posts.postNum}`).empty();
+          comments.forEach((comment)=>{
+            if (comment.commentId == id){
+              $(`#${id}`).append(`<div class='row'><i class="material-icons prefix">subdirectory_arrow_right</i> ${comment.comments}</div>`);
+            }
+          });
+        });
+      },1000);
     });
   });
+});
--- server.js
+++ server.js
@@ -1,14 +1,16 @@
+
+// This is the webserver and end points used in this application.
+
+
 var express = require('express');
 var moment = require('moment');
-const MongoClient = require('mongodb').MongoClient;
-
 var app=express();
-
+const MongoClient = require('mongodb').MongoClient;
 app.use(express.static(__dirname + '/public'));
 
 var log = function(message){
-    var time=moment().format();
-    console.log( `[Server] @ ${time} ${message}`);
+  var time=moment().format();
+  console.log( `[Server] @ ${time} ${message}`);
 }
 
 var port = 3000;
@@ -16,77 +18,68 @@ app.listen(port);
 log(`server listening on: ${port}`);
 
 app.get("/comment",function(req,res){
-    let comment=req.query.comment;
-    let commentId=req.query.commentId;
-    insertComment(comment,commentId);
-    res.send("added");
-})
+  let comment=req.query.comment;
+  let commentId=req.query.commentId;
+  insertComment(comment,commentId);
+  res.send("added");
+});
 
 app.get("/newPost",function(req,res){
-    let user=req.query.user;
-    let image=req.query.image;
-    let caption=req.query.caption;
-    let postNum=req.query.postNum;
-    insertPost(user,image,caption,postNum);
-    res.send("added");
-})
+  let user=req.query.user;
+  let image=req.query.image;
+  let caption=req.query.caption;
+  let postNum=req.query.postNum;
+  insertPost(user,image,caption,postNum);
+  res.send("added");
+});
 
 app.get("/comments", function(req,res){
-    retrieveComment(res);
-   })
+  retrieveComment(res);
+});
 
 app.get("/postnum", function(req,res){
-    retrievePostNum(res);
-   })
+  retrievePostNum(res);
+});
 
-//DataBase Management.
-
+// DataBase Management.
 const uri = "mongodb+srv://sit725:sit725@sit725-assignment1-task.la1a9.mongodb.net/insta?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
-//setting collection variables
+// Setting collection variables
 let collectionMessages;
 let collectionComments;
-
 let collectionPosts;
-// let user;
-// let image;
-// let caption;
-// let comment;
 
-// connecting collection variables to database collections
+
+// Connecting collection variables to database collections
 client.connect(err => {
-    collectionMessages = client.db("insta").collection("messages");
-    collectionComments = client.db("insta").collection("comments");
-    collectionPosts = client.db("insta").collection("posts");
-// let image;
-// let caption;
-// let comment;
-    
-  });
-  
-//insterting into Comment Collection
+  collectionMessages = client.db("insta").collection("messages");
+  collectionComments = client.db("insta").collection("comments");
+  collectionPosts = client.db("insta").collection("posts"); 
+});
+
+// Insterting into Comment Collection
 const insertComment=(comment,commentId)=>{
-   collectionComments.insertOne({comments:comment, commentId:commentId});
-};
+  collectionComments.insertOne({comments:comment, commentId:commentId});
+}
 
-//insterting into Posts Collection
+// Insterting into Posts Collection
 const insertPost=(user,image,caption,postNum)=>{
-    collectionPosts.insertOne({user:user, image:image, caption:caption, postNum:postNum});
- };
+  collectionPosts.insertOne({user:user, image:image, caption:caption, postNum:postNum});
+}
 
-//retreiving from collections
+// Retreiving from collections
 const retrieveComment=(res)=>{
-    collectionComments.find().toArray(function(err,result){
-        if (err) throw err;
-        res.send(result);
-    });
-};
+  collectionComments.find().toArray(function(err,result){
+    if (err) throw err;
+    res.send(result);
+  });
+}
 
-//retreiving Posts
+// Retreiving Posts
 const retrievePostNum=(res)=>{
-    collectionPosts.find().toArray(function(err,result){
-        if (err) throw err;
-        res.send(result);
-    });
-};
+  collectionPosts.find().toArray(function(err,result){
+    if (err) throw err;
+    res.send(result);
+  });
+}
