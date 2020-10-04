33c2e5423eff2ba38ebd6561796d07a9884e9934
Katrina Steen <s.katrina@y7mail.com>
2020-10-04 17:05:38 +1000

    changed to using payloads, made web/package.json specify modules and made changes to keep code running with ES6

---
+++ controllers/documentController.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function () {
+    displayGameBoard();
+    let data = {
+      'handSize': 8,
+      'opponentPlayerNum': 2,
+      'thisPlayerNum': 1
+    }
+   displayHandSize(data);
+  })
+  
--- services/displayServices.js
+++ services/displayServices.js
@@ -1,77 +1,111 @@
+
+
 //hides the join screen and displays the gameboard
-const displayGameBoard=()=> {
-    $("#gameboard").removeClass("hidden");
-    $("#welcomeScreen").addClass("hidden");
+const displayGameBoard = () => {
+ $("#gameboard").removeClass("hidden");
+  $("#welcomeScreen").addClass("hidden");
+
+ // res.send({ result: 200 })
 }
 
 //hides the gameboard & shows the join screen
-const displayWelcomeScreen=()=> {
-    $("#welcomeScreen").removeClass("hidden");
-    $("#gameboard").addClass("hidden");
+const displayWelcomeScreen = () => {
+  $("#welcomeScreen").removeClass("hidden");
+  $("#gameboard").addClass("hidden");
+  //res.send({ result: 200 })
 }
 
 //gets player position (used to determine which card/bullets to adjust on screen)
 function getPlayerPosition(opponentPlayerNum, thisPlayerNum) {
-    let playerDiff = opponentPlayerNum - thisPlayerNum;
-    let position = "unavailable";
+  let playerDiff = opponentPlayerNum - thisPlayerNum;
+  let position = "unavailable";
 
-    switch (playerDiff) {
-        case 1:
-        case -4:
-            position = "b5";
-            break;
-        case 2:
-        case -3:
-            position = "c5";
-            break;
-        case 3:
-        case -2:
-            position = "d5";
-            break;
-        case 4:
-        case -1:
-            position = "e5";
-            break;
-        default:
-            position = "unavailable";
-    }
-    return position;
+  switch (playerDiff) {
+    case 0:
+      position = "a5";
+      break;
+    case 1:
+    case -4:
+      position = "b5";
+      break;
+    case 2:
+    case -3:
+      position = "c5";
+      break;
+    case 3:
+    case -2:
+      position = "d5";
+      break;
+    case 4:
+    case -1:
+      position = "e5";
+      break;
+    default:
+      position = "unavailable";
+  }
+  return position;
 }
 
-//displays an opponents handsize on screen - needs size, opponent player number & current player number
-const displayHandSize=(handSize, opponentPlayerNum, thisPlayerNum) =>{
-    let position = getPlayerPosition(opponentPlayerNum, thisPlayerNum);
-    if (position != "unavailable") {
-
-        let divString = "";
-        if (handSize > 0) {
-            divString += '<img src="assets/cardBack.png" class="responsive">';
-            for (i = 1; i < handSize; i++) {
-                divString += '<img src="assets/cardOverlap.png"class="responsive">';
-            }
-            let jqString = '#' + position + ' .privateHand';
-            $(jqString).html(divString);
+//displays an opponents handsize on screen (payload needs handSize, opponentPlayerNum & thisPlayerNum)
+const displayHandSize = (payload) => {
+  let position = getPlayerPosition(payload.opponentPlayerNum, payload.thisPlayerNum);
+  if (position != "unavailable") {
+    let divString = "";
+    //(update not applicable if position is this player's)
+    if (position != "a1") {
+      if (payload.handSize > 0) {
+        divString += '<img src="assets/cardBack.png" class="responsive">';
+        for (i = 1; i < payload.handSize; i++) {
+          divString += '<img src="assets/cardOverlap.png"class="responsive">';
         }
-    } else {
-    alert("error encountered: hand size not available");
+        let jqString = '#' + position + ' .privateHand';
+        $(jqString).html(divString);
+      }
     }
+    //res.send({ result: 200 });
+  }
+  else {
+   // res.send({ result: "position unable to be determined" });
+  }
 }
 
-module.exports={
-    displayHandSize,
-    displayGameBoard,
-    displayWelcomeScreen
+/*
+$(document).ready(function () {
+  displayGameBoard();
+  let data = {
+    'handSize': 8,
+    'opponentPlayerNum': 2,
+    'thisPlayerNum': 1
+  }
+ displayHandSize(data);
+})
+*/
+
+/*
+module.exports = {
+  displayHandSize,
+  displayGameBoard,
+  displayWelcomeScreen
 }
+*/
+
 
 /*
 in file to use...
 const display = require('../services/displayServices')
 then:
-display.displayGameBoard, etc, in code 
+display.displayGameBoard, etc, in code
 
 eg.
+
+
 $(document).ready(function () {
     display.displayGameBoard();
-    display.displayHandSize(1, 5, 2);
+    data {
+      "handSize":5,
+      "opponentPlayerNum":2,
+      "thisPlayerNum": 1
+    }
+    display.displayHandSize(data, res);
 })
 */
